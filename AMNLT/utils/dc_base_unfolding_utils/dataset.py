74cbd53e0d03b4573d6116378dbbb93442ca9592
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-01-01 17:32:46 +0100

    Rework dc_base_unfolding Dataset vocabulary making

--- AMNLT/utils/dc_base_unfolding_utils/dataset.py
+++ AMNLT/utils/dc_base_unfolding_utils/dataset.py
@@ -14,8 +14,141 @@ from AMNLT.utils.dc_base_unfolding_utils.data_preprocessing import (
     preprocess_transcript,
 )
 
+from gabcparser import GabcParser
+from gabcparser.utils import separate_lyrics_music
+import gabcparser.grammars as grammars
+
+from enum import Enum
+
 ################################################################################################ Single-source:
 
+class Separation(Enum):
+    NONE = None
+    LYRIC = "lyric"
+    MUSIC = "music"
+
+# ds_name = name of HuggingFace dataset
+# encoding_type = ["char", "new_gabc", "music_aware"]
+# transcription_separation = [None, "lyric", "music"]
+def make_vocabulary(ds_name, encoding_type, transcription_separation: Separation = Separation.NONE):
+        vocab = set()
+        ds = load_dataset(ds_name)
+
+        if transcription_separation != Separation.NONE:
+            if ds_name == "PRAIG/GregoSynth_staffLevel":
+                gabc_variation = grammars.GABC
+            elif ds_name == "PRAIG/Solesmes_staffLevel":
+                gabc_variation = grammars.S_GABC
+            elif ds_name in ["PRAIG/Einsiedeln_staffLevel", "PRAIG/Salzinnes_staffLevel"]:
+                gabc_variation = grammars.MEI_GABC
+            else:
+                raise ValueError(f"Could not infer gabc variation for the unknown dataset '{ds_name}'")
+        
+            parser = GabcParser.load_parser(gabc_variation)
+
+            error_indices = []
+            for split in ds.keys():
+                ds[split]["transcription_lyric"] = []
+                ds[split]["transcription_music"] = []
+                for i,transcript in enumerate(ds[split]["transcription"]):
+                    lyric, music = separate_lyrics_music.separate_lyrics_music(transcript, parser)
+                    if lyric is None or music is None:
+                        error_indices.append(i)
+                    ds[split]["transcription_lyric"] = [].append(lyric)
+                    ds[split]["transcription_music"] = [].append(music)
+            if len(error_indices) > 0:
+                print(f"Could not separate lyrics and musics for the following samples: {error_indices}")
+
+        key = CTCDataset.TRANSCRIPT
+        if transcription_separation == Separation.LYRIC:
+            key = "transcription_lyric"
+        elif transcription_separation == Separation.MUSIC:
+            key = "transcription_music"
+
+        if (encoding_type == "char") or (
+            encoding_type == "new_gabc" 
+            and ds_name in ["PRAIG/Einsiedeln_staffLevel", "PRAIG/Salzinnes_staffLevel"] 
+            and transcription_separation == Separation.LYRIC
+            ):
+            for split in ds.keys():
+                for y in ds[split][key]:
+                    vocab.update(y.strip())
+        elif encoding_type == "music_aware":
+            for split in ds.keys():
+                for y in ds[split][CTCDataset.TRANSCRIPT]:
+                    # Leer todo el contenido del archivo
+                    content = y.strip()
+                    i = 0
+                    while i < len(content):
+                        if content[i:i+3] == "<m>":  # Check if the token starts with the musical tag
+                            # If so, extract the musical character with the tag
+                            if i + 3 < len(content):  # Ensure it doesn't go out of range
+                                vocab.add("<m>" + content[i+3])
+                                i += 3  # Skip to the next character after <m>
+                            else:
+                                break
+                        else:
+                            # Add the normal character to the vocabulary
+                            vocab.add(content[i])
+                        i += 1
+        elif encoding_type == "new_gabc" and ds_name in ["PRAIG/Einsiedeln_staffLevel", "PRAIG/Salzinnes_staffLevel"]:
+            for split in ds.keys():
+                for y in ds[split][CTCDataset.TRANSCRIPT]:
+                    content = y
+                    i = 0
+                    while i < len(content):
+                        if content[i] == '(':
+                            # Tokenize '('
+                            vocab.add(content[i])
+                            i += 1
+                            # Collect the characters inside the parentheses
+                            temp = ''
+                            while i < len(content) and content[i] != ')':
+                                temp += content[i]
+                                i += 1
+                            # Split the collected characters by spaces and add them as tokens
+                            for token in temp.split():
+                                vocab.add(token)
+                            # Tokenize ')'
+                            if i < len(content):
+                                vocab.add(content[i])
+                                i += 1
+                        else:
+                            # Tokenize each character outside parentheses
+                            vocab.add(content[i])
+                            i += 1
+                            
+        elif encoding_type == "new_gabc" and ds_name in ["PRAIG/Einsiedeln_staffLevel", "PRAIG/Salzinnes_staffLevel"] and transcription_separation == Separation.MUSIC:
+            for split in ds.keys():
+                for y in ds[split][key]:
+                    content = y.strip()
+                    i = 0
+                    temp = ''
+                    while i < len(content):
+                        if content[i] == " " or content[i] == ")":
+                            if temp:
+                                vocab.add(temp)
+                            vocab.add(content[i])
+                            temp = ''
+                        elif content[i] == "(":
+                            vocab.add(content[i])
+                        else:
+                            temp += content[i]
+                        i += 1
+                    if temp:
+                        vocab.add(temp)
+                    
+        vocab = sorted(vocab)
+
+        w2i = {}
+        i2w = {}
+        for i, w in enumerate(vocab):
+            w2i[w] = i + 1
+            i2w[i + 1] = w
+        w2i["<PAD>"] = 0
+        i2w[0] = "<PAD>"
+
+        return w2i, i2w
 
 class CTCDataset(Dataset):
     IMAGE = "image"
@@ -49,7 +182,7 @@ class CTCDataset(Dataset):
         vocab_folder = os.path.join(os.path.join("data", self.name), "vocab")
         os.makedirs(vocab_folder, exist_ok=True)
         self.w2i_path = os.path.join(vocab_folder, vocab_name)
-        self.w2i, self.i2w = self.check_and_retrieve_vocabulary(ds_name)
+        self.w2i, self.i2w = self.check_and_retrieve_vocabulary()
 
     def __len__(self):
         return len(self.X)
@@ -128,7 +261,7 @@ class CTCDataset(Dataset):
         ds = load_dataset(ds_name, split=split)
         return ds[self.IMAGE], ds[self.TRANSCRIPT]
 
-    def check_and_retrieve_vocabulary(self, ds_name):
+    def check_and_retrieve_vocabulary(self):
         w2i = {}
         i2w = {}
 
@@ -137,93 +270,12 @@ class CTCDataset(Dataset):
                 w2i = json.load(file)
             i2w = {v: k for k, v in w2i.items()}
         else:
-            w2i, i2w = self.make_vocabulary(ds_name)
+            w2i, i2w = self.make_vocabulary()
             with open(self.w2i_path, "w") as file:
                 json.dump(w2i, file)
 
         return w2i, i2w
     
-    def make_vocabulary(self, ds_name):
-        vocab = set()
-        ds = load_dataset(ds_name)
-        print(self.encoding_type)
-        if (self.encoding_type == "char") or (self.encoding_type == "new_gabc" and self.name in ["einsiedeln_lyrics", "salzinnes_lyrics"]):
-            for split in ds.keys():
-                for y in ds[split][self.TRANSCRIPT]:
-                    vocab.update(y.strip())
-        elif self.encoding_type == "music_aware":
-            for split in ds.keys():
-                for y in ds[split][self.TRANSCRIPT]:
-                    # Leer todo el contenido del archivo
-                    content = y.strip()
-                    i = 0
-                    while i < len(content):
-                        if content[i:i+3] == "<m>":  # Check if the token starts with the musical tag
-                            # If so, extract the musical character with the tag
-                            if i + 3 < len(content):  # Ensure it doesn't go out of range
-                                vocab.add("<m>" + content[i+3])
-                                i += 3  # Skip to the next character after <m>
-                            else:
-                                break
-                        else:
-                            # Add the normal character to the vocabulary
-                            vocab.add(content[i])
-                        i += 1
-        elif self.encoding_type == "new_gabc" and self.name in ["einsiedeln", "salzinnes", "PRAIG/Einsiedeln_staffLevel", "PRAIG/Salzinnes_staffLevel"]:
-            for split in ds.keys():
-                for y in ds[split][self.TRANSCRIPT]:
-                    content = y
-                    i = 0
-                    while i < len(content):
-                        if content[i] == '(':
-                            # Tokenize '('
-                            vocab.add(content[i])
-                            i += 1
-                            # Collect the characters inside the parentheses
-                            temp = ''
-                            while i < len(content) and content[i] != ')':
-                                temp += content[i]
-                                i += 1
-                            # Split the collected characters by spaces and add them as tokens
-                            for token in temp.split():
-                                vocab.add(token)
-                            # Tokenize ')'
-                            if i < len(content):
-                                vocab.add(content[i])
-                                i += 1
-                        else:
-                            # Tokenize each character outside parentheses
-                            vocab.add(content[i])
-                            i += 1
-                            
-        elif self.encoding_type == "new_gabc" and self.name in ["einsiedeln_music", "salzinnes_music"]:
-            for split in ds.keys():
-                for y in ds[split][self.TRANSCRIPT]:
-                    content = y.strip()
-                    i = 0
-                    temp = ''
-                    while i < len(content):
-                        if content[i] == " " or content[i] == ")":
-                            if temp:
-                                vocab.add(temp)
-                            vocab.add(content[i])
-                            temp = ''
-                        elif content[i] == "(":
-                            vocab.add(content[i])
-                        else:
-                            temp += content[i]
-                        i += 1
-                    if temp:
-                        vocab.add(temp)
-                    
-        vocab = sorted(vocab)
-
-        w2i = {}
-        i2w = {}
-        for i, w in enumerate(vocab):
-            w2i[w] = i + 1
-            i2w[i + 1] = w
-        w2i["<PAD>"] = 0
-        i2w[0] = "<PAD>"
-
-        return w2i, i2w
+    def make_vocabulary(self):
+        # TODO: add support for transcription separation
+        return make_vocabulary(self.name, self.encoding_type)
