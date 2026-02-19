42db7f7f0e9fa5f2d4688b2dbecd5ec3a674f560
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-02-19 16:20:36 +0100

    smt_dan: add early stopping (as per paper)

--- AMNLT/scripts/smt_dan/train_dan.py
+++ AMNLT/scripts/smt_dan/train_dan.py
@@ -13,7 +13,7 @@ from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 torch.set_float32_matmul_precision('high')
 
-def main(config_path):
+def main(config_path, patience):
     
     with open(config_path, "r") as f:
         config = experiment_config_from_dict(json.load(f))
@@ -34,10 +34,21 @@ def main(config_path):
     checkpointer = ModelCheckpoint(dirpath=f"weights/{dataset_name}/", filename=f"{dataset_name}_DAN", 
                                    monitor="val_CER", mode='min',
                                    save_top_k=1, verbose=True)
+    early_stopper = EarlyStopping(
+            monitor="val_CER",
+            min_delta=0.1,
+            patience=patience,
+            verbose=True,
+            mode="min",
+            strict=True,
+            check_finite=True,
+            divergence_threshold=100.00,
+            check_on_train_epoch_end=False,
+        )
 
     trainer = Trainer(max_epochs=10000, 
                       check_val_every_n_epoch=5, 
-                      logger=wandb_logger, callbacks=[checkpointer],
+                      logger=wandb_logger, callbacks=[checkpointer, early_stopper],
                       precision="16-mixed")
     
     trainer.fit(model_wrapper,datamodule=datamodule)
@@ -46,8 +57,8 @@ def main(config_path):
 
     trainer.test(model, datamodule=datamodule)
 
-def launch(config_path):
-    main(config_path)
+def launch(config_path, patience = 5):
+    main(config_path, patience)
 
 if __name__ == "__main__":
     fire.Fire(launch)
